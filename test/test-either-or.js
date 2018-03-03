744dffcaba66b787aba28fcbe0a3559710bb6296
Philippe Bernard <philippe.bernard@gmail.com>
2018-03-03 01:11:03 +0100

    'if' support in either/or

--- test/test-either-or.js
+++ test/test-either-or.js
@@ -467,4 +467,511 @@ describe('EitherOr', function() {
       1, 3, 5, 7, 8, 9, 10
     ]);
   });
+
+  it('should enumerate branches with either... or... and conditions - 01', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+      eo.either({if: false}, function() {
+        trace.push(2);
+      }).or(function() {
+        trace.push(3);
+      });
+      trace.push(4);
+    });
+    assert.deepEqual(trace, [
+      1, 3, 4
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 02', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+      eo.either(function() {
+        trace.push(2);
+      }).or({if: false}, function() {
+        trace.push(3);
+      });
+      trace.push(4);
+    });
+    assert.deepEqual(trace, [
+      1, 2, 4
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 03', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+      eo.either({if: false}, function() {
+        trace.push(2);
+      }).or(function() {
+        trace.push(3);
+      }).or(function() {
+        trace.push(4);
+      });
+      trace.push(5);
+    });
+    assert.deepEqual(trace, [
+      1, 3, 5,
+      1, 4, 5
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 04', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+      eo.either(function() {
+        trace.push(2);
+      }).or({if: false}, function() {
+        trace.push(3);
+      }).or(function() {
+        trace.push(4);
+      });
+      trace.push(5);
+    });
+    assert.deepEqual(trace, [
+      1, 2, 5,
+      1, 4, 5
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 05', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+      eo.either(function() {
+        trace.push(2);
+      }).or(function() {
+        trace.push(3);
+      }).or({if: false}, function() {
+        trace.push(4);
+      });
+      trace.push(5);
+    });
+    assert.deepEqual(trace, [
+      1, 2, 5,
+      1, 3, 5
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 06', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+      eo.either({if: false}, function() {
+        trace.push(2);
+      }).or({if: false}, function() {
+        trace.push(3);
+      }).or(function() {
+        trace.push(4);
+      });
+      trace.push(5);
+    });
+    assert.deepEqual(trace, [
+      1, 4, 5
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 07', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+      eo.either({if: false}, function() {
+        trace.push(2);
+      }).or(function() {
+        trace.push(3);
+      }).or({if: false}, function() {
+        trace.push(4);
+      });
+      trace.push(5);
+    });
+    assert.deepEqual(trace, [
+      1, 3, 5
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 08', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+      eo.either(function() {
+        trace.push(2);
+      }).or({if: false}, function() {
+        trace.push(3);
+      }).or({if: false}, function() {
+        trace.push(4);
+      });
+      trace.push(5);
+    });
+    assert.deepEqual(trace, [
+      1, 2, 5
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 09', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+
+      eo.either({if: false}, function() {
+        trace.push(2);
+      }).or(function() {
+        trace.push(3);
+      });
+
+      trace.push(4);
+
+      eo.either(function() {
+        trace.push(5);
+      }).or(function() {
+        trace.push(6);
+      });
+
+      trace.push(7);
+    });
+    assert.deepEqual(trace, [
+      1, 3, 4, 5, 7,
+      1, 3, 4, 6, 7
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 10', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+
+      eo.either(function() {
+        trace.push(2);
+      }).or({if: false}, function() {
+        trace.push(3);
+      });
+
+      trace.push(4);
+
+      eo.either(function() {
+        trace.push(5);
+      }).or(function() {
+        trace.push(6);
+      });
+
+      trace.push(7);
+    });
+    assert.deepEqual(trace, [
+      1, 2, 4, 5, 7,
+      1, 2, 4, 6, 7
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 11', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+
+      eo.either(function() {
+        trace.push(2);
+      }).or(function() {
+        trace.push(3);
+      });
+
+      trace.push(4);
+
+      eo.either({if: false}, function() {
+        trace.push(5);
+      }).or(function() {
+        trace.push(6);
+      });
+
+      trace.push(7);
+    });
+    assert.deepEqual(trace, [
+      1, 2, 4, 6, 7,
+      1, 3, 4, 6, 7
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 12', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+
+      eo.either(function() {
+        trace.push(2);
+      }).or(function() {
+        trace.push(3);
+      });
+
+      trace.push(4);
+
+      eo.either(function() {
+        trace.push(5);
+      }).or({if: false}, function() {
+        trace.push(6);
+      });
+
+      trace.push(7);
+    });
+    assert.deepEqual(trace, [
+      1, 2, 4, 5, 7,
+      1, 3, 4, 5, 7
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 13', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+
+      eo.either({if: false}, function() {
+        trace.push(2);
+      }).or(function() {
+        trace.push(3);
+      });
+
+      trace.push(4);
+
+      eo.either({if: false}, function() {
+        trace.push(5);
+      }).or(function() {
+        trace.push(6);
+      });
+
+      trace.push(7);
+    });
+    assert.deepEqual(trace, [
+      1, 3, 4, 6, 7
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 14', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+
+      eo.either({if: false}, function() {
+        trace.push(2);
+      }).or(function() {
+        trace.push(3);
+      });
+
+      trace.push(4);
+
+      eo.either(function() {
+        trace.push(5);
+      }).or({if: false}, function() {
+        trace.push(6);
+      });
+
+      trace.push(7);
+    });
+    assert.deepEqual(trace, [
+      1, 3, 4, 5, 7
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 15', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+
+      eo.either(function() {
+        trace.push(2);
+      }).or({if: false}, function() {
+        trace.push(3);
+      });
+
+      trace.push(4);
+
+      eo.either({if: false}, function() {
+        trace.push(5);
+      }).or(function() {
+        trace.push(6);
+      });
+
+      trace.push(7);
+    });
+    assert.deepEqual(trace, [
+      1, 2, 4, 6, 7
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 16', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+
+      eo.either(function() {
+        trace.push(2);
+      }).or({if: false}, function() {
+        trace.push(3);
+      });
+
+      trace.push(4);
+
+      eo.either(function() {
+        trace.push(5);
+      }).or({if: false}, function() {
+        trace.push(6);
+      });
+
+      trace.push(7);
+    });
+    assert.deepEqual(trace, [
+      1, 2, 4, 5, 7
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 17', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+      eo.either(function() {
+        trace.push(2);
+        eo.either(function() {
+          trace.push(3);
+        }).or({if: false}, function() {
+          trace.push(4);
+        });
+      }).or({if: false}, function() {
+        trace.push(5);
+      });
+      trace.push(6);
+    });
+    assert.deepEqual(trace, [
+      1, 2, 3, 6
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 18', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+      eo.either(function() {
+        trace.push(2);
+        eo.either(function() {
+          trace.push(3);
+        }).or({if: false}, function() {
+          trace.push(4);
+        });
+      }).or(function() {
+        trace.push(5);
+      });
+      trace.push(6);
+    });
+    assert.deepEqual(trace, [
+      1, 2, 3, 6,
+      1, 5, 6
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 19', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+      eo.either(function() {
+        trace.push(2);
+        eo.either({if: false}, function() {
+          trace.push(3);
+        }).or(function() {
+          trace.push(4);
+        });
+      }).or({if: false}, function() {
+        trace.push(5);
+      });
+      trace.push(6);
+    });
+    assert.deepEqual(trace, [
+      1, 2, 4, 6
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 20', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+      eo.either(function() {
+        trace.push(2);
+        eo.either({if: false}, function() {
+          trace.push(3);
+        }).or(function() {
+          trace.push(4);
+        });
+      }).or(function() {
+        trace.push(5);
+      });
+      trace.push(6);
+    });
+    assert.deepEqual(trace, [
+      1, 2, 4, 6,
+      1, 5, 6
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 21', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+      eo.either({if: false}, function() {
+        trace.push(2);
+      }).or(function() {
+        trace.push(3);
+        eo.either(function() {
+          trace.push(4);
+        }).or({if: false}, function() {
+          trace.push(5);
+        });
+        trace.push(6);
+      });
+      trace.push(7);
+    });
+    assert.deepEqual(trace, [
+      1, 3, 4, 6, 7
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 22', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+      eo.either(function() {
+        trace.push(2);
+      }).or(function() {
+        trace.push(3);
+        eo.either(function() {
+          trace.push(4);
+        }).or({if: false}, function() {
+          trace.push(5);
+        });
+        trace.push(6);
+      });
+      trace.push(7);
+    });
+    assert.deepEqual(trace, [
+      1, 2, 7,
+      1, 3, 4, 6, 7
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 23', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+      eo.either({if: false}, function() {
+        trace.push(2);
+      }).or(function() {
+        trace.push(3);
+        eo.either({if: false}, function() {
+          trace.push(4);
+        }).or(function() {
+          trace.push(5);
+        });
+        trace.push(6);
+      });
+      trace.push(7);
+    });
+    assert.deepEqual(trace, [
+      1, 3, 5, 6, 7
+    ]);
+  });
+
+  it('should enumerate branches with either... or... and conditions - 24', function() {
+    EitherOr.runAll(function(eo) {
+      trace.push(1);
+      eo.either(function() {
+        trace.push(2);
+      }).or(function() {
+        trace.push(3);
+        eo.either({if: false}, function() {
+          trace.push(4);
+        }).or(function() {
+          trace.push(5);
+        });
+        trace.push(6);
+      });
+      trace.push(7);
+    });
+    assert.deepEqual(trace, [
+      1, 2, 7,
+      1, 3, 5, 6, 7
+    ]);
+  });
 });
